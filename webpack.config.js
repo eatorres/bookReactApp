337f5744ab84e12f0ae5633eabfd36077c38025b
eatorres <etorres_57@yahoo.com>
2017-07-09 15:43:53 -0400

    added bootstrap

--- webpack.config.js
+++ webpack.config.js
@@ -3,5 +3,18 @@ module.exports = {
     output: {
         path: __dirname + '/build',
         filename: 'bundle.js'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.jsx?$/,
+                loader: 'babel-loader',
+                exclude: __dirname + './node_modules',
+                query: {
+                    presets: ['es2015', 'react']
+                }
+            }
+        ]
     }
+
 };
